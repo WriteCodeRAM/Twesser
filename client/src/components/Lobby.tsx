5dbf27ef806c8ee4023a82a7c429ea1c39361c64
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-05-06 19:02:06 -0400

    TODO: abstract socket logic and display members inside lobby

--- client/src/components/Lobby.tsx
+++ client/src/components/Lobby.tsx
@@ -1,12 +1,20 @@
 import { SocketOptions } from "socket.io-client";
+import { useEffect } from "react";
 
 interface LobbyProps {
-  socket: SocketOptions;
+  socket: any;
   username: string;
   room: string;
 }
 
 function Lobby({ socket, username, room }: LobbyProps) {
-  return <div>Lobby</div>;
+  socket.emit("get_members", { room });
+
+  useEffect(() => {
+    socket.on("get_members", (members: any) => {
+      console.log("test");
+    });
+  }, []);
+  return <div className="bg-white">Lobby</div>;
 }
 export default Lobby;
