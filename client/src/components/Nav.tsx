7a5da88b294fb0fb5935e17a6f3edc3e396f1963
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-05-06 18:52:12 -0400

    made Nav redirect to home page

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
 const Nav = () => {
   return (
     <nav className="flex justify-center">
-      <h1 className="text-dark-gray font-madimi font-bold text-3xl py-2 px-4">
-        Twesser
-      </h1>
+      <Link href={"/"}>
+        <h1 className="text-dark-gray font-madimi font-bold text-3xl py-2 px-4">
+          Twesser
+        </h1>
+      </Link>
     </nav>
   );
 };
