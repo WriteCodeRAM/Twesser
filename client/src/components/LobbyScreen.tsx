8af5cfd58cb7468d1a6828f65fa645f60aa75ab5
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-06-24 12:41:55 -0400

    added error to props

--- client/src/components/LobbyScreen.tsx
+++ client/src/components/LobbyScreen.tsx
@@ -1,11 +1,25 @@
-function LobbyScreen() {
+import { socket } from "@/app/socket";
+import { useState } from "react";
+import { useEffect } from "react";
+import { prisma } from "../lib/prisma";
+
+const LobbyScreen = ({ questions, error }) => {
+  // tweets state (eventually fetch 5 items from db)
+
   return (
     <div className="w-full">
-      <div className="w-4/5 bg-white h-64 text-center shadow-lg rounded-lg p-6">
-        <h1 className="text-xl font-semibold">Lobby Screen</h1>
+      <div className="bg-white h-64 text-center shadow-lg rounded-lg p-6">
+        {error ? (
+          <h1 className="text-xl font-semibold text-muted-red">{error}</h1>
+        ) : (
+          <h1 className="text-xl font-semibold">
+            Waiting on host to start game
+          </h1>
+        )}
+        {/* <p>Hello, {tweets?.answer}</p> */}
       </div>
     </div>
   );
-}
+};
 
 export default LobbyScreen;
