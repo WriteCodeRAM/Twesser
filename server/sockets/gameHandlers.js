26180bd678b75f88ca6286793c2aff5238c5eed8
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-06-25 11:24:58 -0400

    fixed issue where game started event  was not sent to all clients

--- server/sockets/gameHandlers.js
+++ server/sockets/gameHandlers.js
@@ -2,11 +2,16 @@ module.exports = (io, socket, rooms) => {
   socket.on("start_game", ({ room }) => {
     console.log(socket.id);
     console.log(rooms[room]);
-    console.log(`Attempting to start game in room: ${room}`);
-    if (rooms[room] && rooms[room].members.length > 1) {
+    console.log(`Attempting to start game in room: ${room}`); //if user is host start game
+    const host = rooms[room].members.filter((member) => member.host === true);
+
+    if (host[0].id !== socket.id) {
+      socket.emit("host_must_start_game");
+    } else if (rooms[room] && rooms[room].members.length > 1) {
       // Your logic here
       console.log("game started");
       // emit listener to client
+      io.emit("game_started");
       // sound effect is possible
       // show on lobby screen a countdown
       // during the countdown I want to fetch the tweets from db
