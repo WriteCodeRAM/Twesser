f11a50938a923432a9ae71baced1ca4f98b36b7f
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-03 13:28:46 -0400

    abstracted game rules and errors  away from lobby and into custom hook

---
+++ client/src/app/hooks/useGameRules.ts
@@ -0,0 +1,38 @@
+import { useState, useEffect } from "react";
+import { socket } from "../socket";
+
+export const useGameRules = () => {
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const handlePlayerCountWarning = () => {
+      setError("Game requires at least 2 players to start.");
+      setTimeout(() => {
+        setError("");
+      }, 2000);
+    };
+
+    const handleHostMustStartGame = () => {
+      setError("Only the host of the room is allowed to start the game.");
+      setTimeout(() => {
+        setError("");
+      }, 2000);
+    };
+
+    const handleGameStarting = () => {
+      console.log("in game starting");
+      setError("Game starting...");
+    };
+
+    socket.on("player_count_warning", handlePlayerCountWarning);
+    socket.on("host_must_start_game", handleHostMustStartGame);
+    socket.on("game_started", handleGameStarting);
+
+    return () => {
+      socket.off("player_count_warning", handlePlayerCountWarning);
+      socket.off("host_must_start_game", handleHostMustStartGame);
+    };
+  }, []);
+
+  return { error };
+};
