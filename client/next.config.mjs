83ad0b53f54de52b17798593bbb53c06ae84aa5f
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-06 23:59:08 -0400

    added utfs to remotePatterns for next image

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+export default {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/f/**",
+      },
+    ],
+  },
+};
