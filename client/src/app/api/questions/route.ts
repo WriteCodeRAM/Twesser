13b9f446072d59e36d4db5165b788991376ca141
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-07 00:00:15 -0400

    route to fetch questions

--- client/src/app/api/questions/route.ts
+++ client/src/app/api/questions/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 
+// make sure whatever questions retrieved arent repeats
+//create history for rooms
+// filter by ids
+//findMany() gets all records so maybe do that and then use a loop to populate the new question pool to make sure
 export async function GET() {
   const questions = await prisma.questions.findMany({
     take: 10,
