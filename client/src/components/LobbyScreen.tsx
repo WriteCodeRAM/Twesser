3c5aba2260de20773e666f28b9571c0f996de6ee
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-07 00:08:01 -0400

    conditionally rendering the question based off gameStarted hook

--- client/src/components/LobbyScreen.tsx
+++ client/src/components/LobbyScreen.tsx
@@ -1,23 +1,26 @@
-import { socket } from "@/app/socket";
-import { useState } from "react";
-import { useEffect } from "react";
-import { prisma } from "../lib/prisma";
+import { useGetQuestions } from "@/app/hooks/useGetQuestions";
+import { useSoundEffects } from "@/app/hooks/useSoundEffects";
+import Image from "next/image";
 
-const LobbyScreen = ({ questions, error }) => {
-  // tweets state (eventually fetch 5 items from db)
-
+const LobbyScreen = ({ error, img }) => {
+  const { gameStarted } = useSoundEffects();
   return (
-    <div className="w-full">
-      <div className="bg-white h-64 text-center shadow-lg rounded-lg p-6">
-        {error ? (
-          <h1 className="text-xl font-semibold text-muted-red">{error}</h1>
-        ) : (
-          <h1 className="text-xl font-semibold">
-            Waiting on host to start game
-          </h1>
-        )}
-        {/* <p>Hello, {tweets?.answer}</p> */}
-      </div>
+    <div className="">
+      {!gameStarted ? (
+        <div className="bg-white h-64 text-center shadow-lg rounded-lg p-6">
+          {error ? (
+            <h1 className="text-xl font-semibold text-muted-red">{error}</h1>
+          ) : (
+            <h1 className="text-xl font-semibold">
+              Waiting on host to start game
+            </h1>
+          )}
+        </div>
+      ) : (
+        <div className="image-container">
+          <Image src={img} width={800} height={350} alt="" />
+        </div>
+      )}
     </div>
   );
 };
