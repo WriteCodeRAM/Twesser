e92f83de3715e7800c30e0fc7ebd2eacb8837992
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-19 00:38:02 -0400

    added score property, working on scoring system

--- server/sockets/gameHandlers.js
+++ server/sockets/gameHandlers.js
@@ -1,6 +1,5 @@
 module.exports = (io, socket, rooms) => {
   socket.on("start_game", ({ room }) => {
-    console.log(`Attempting to start game in room: ${room}`);
     // find host
     const host = rooms[room].members.filter((member) => member.host === true);
     // dont allow regular members to start game
@@ -10,13 +9,12 @@ module.exports = (io, socket, rooms) => {
       console.log("game started");
       // emit listener to room
       io.to(room).emit("game_started");
-      // sound effect is possible
-      // show on lobby screen a countdown
-      // during the countdown I want to fetch the tweets from db
-      // once countdown is finished show first blurred tweet
     } else {
       console.log(`Game requires at least 2 people to play.`);
       socket.emit("player_count_warning");
     }
+  });
+  socket.on("end_round", (room) => {
+    io.to(room).emit("increment_index");
   });
 };
