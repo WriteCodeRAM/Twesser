e3eb9d2af11de6940c8884a4faf7d0d63299ea3c
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-19 00:05:34 -0400

    cleaned up component to conditionally render different screens based on round progress

--- client/src/components/LobbyScreen.tsx
+++ client/src/components/LobbyScreen.tsx
@@ -1,24 +1,80 @@
-import { useGetQuestions } from "@/app/hooks/useGetQuestions";
-import { useSoundEffects } from "@/app/hooks/useSoundEffects";
+import React from "react";
+import AnswerChoices from "./AnswerChoices";
 import Image from "next/image";
+import Leaderboard from "./Leaderboard";
 
-const LobbyScreen = ({ error, img }) => {
-  const { gameStarted } = useSoundEffects();
-  return (
-    <div className="">
-      {!gameStarted ? (
-        <div className="bg-white h-64 text-center shadow-lg rounded-lg p-6">
-          {error ? (
-            <h1 className="text-xl font-semibold text-muted-red">{error}</h1>
-          ) : (
-            <h1 className="text-xl font-semibold">
-              Waiting on host to start game
-            </h1>
-          )}
-        </div>
+type Question = {
+  id: number;
+  answerChoices: string[];
+  blurredURL: string;
+  unblurredURL: string;
+  answer: string;
+};
+
+interface LobbyScreenProps {
+  error: string;
+  data: Question | null;
+  gameStarted: boolean;
+  roundStarted: boolean;
+  roundOver: boolean;
+  intermission: boolean;
+}
+
+const LobbyScreen = ({
+  error,
+  data,
+  gameStarted,
+  roundStarted,
+  roundOver,
+  intermission,
+}: LobbyScreenProps) => {
+  const renderWaiting = () => (
+    <div className="rounded-lg bg-white p-6 text-center shadow-lg">
+      {error ? (
+        <h1 className="text-xl font-semibold text-muted-red">{error}</h1>
       ) : (
-        <div className="image-container">
-          <Image src={img} width={800} height={350} alt="" />
+        <h1 className="text-xl font-semibold">Waiting on host to start game</h1>
+      )}
+    </div>
+  );
+
+  const renderRoundStarted = (data: Question) => (
+    <div className="image-container">
+      <Image
+        src={data.blurredURL}
+        width={800}
+        height={350}
+        alt=""
+        priority={true}
+      />
+      <AnswerChoices choices={data.answerChoices} />
+    </div>
+  );
+
+  const renderRoundOver = (data: Question) => (
+    <div className="image-container">
+      <Image
+        src={data.unblurredURL}
+        width={800}
+        height={350}
+        alt=""
+        priority={true}
+      />
+      <h1 className="text-center text-xl font-semibold">{data.answer}</h1>
+    </div>
+  );
+
+  const renderIntermission = () => <Leaderboard />;
+
+  return (
+    <div className="size-full">
+      {!gameStarted ? (
+        renderWaiting()
+      ) : (
+        <div className="flex flex-col justify-center gap-4 align-middle">
+          {roundStarted && data && renderRoundStarted(data)}
+          {roundOver && data && renderRoundOver(data)}
+          {intermission && renderIntermission()}
         </div>
       )}
     </div>
