3fe31dcba4648a5a09e858f0ae6ccfb7746aa8f0
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-18 23:56:10 -0400

    added room as param, logic to show leaderboard and automated game flow

--- client/src/app/hooks/useSoundEffects.ts
+++ client/src/app/hooks/useSoundEffects.ts
@@ -1,31 +1,55 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { socket } from "../socket";
 
-export const useSoundEffects = () => {
+export const useSoundEffects = (room: string) => {
   const [gameStarted, setGameStarted] = useState(false);
+  const [roundStarted, setRoundStarted] = useState(false);
+  const [roundOver, setRoundOver] = useState(false);
+  const [intermission, setIntermission] = useState(false);
+
+  const countdown = useRef<HTMLAudioElement | null>(null);
+  const roundStart = useRef<HTMLAudioElement | null>(null);
+  const roundMusic = useRef<HTMLAudioElement | null>(null);
+  const endRound = useRef<HTMLAudioElement | null>(null);
 
   useEffect(() => {
-    const countdown = new Audio("/audio/countdown.mp3");
-    const roundStart = new Audio("/audio/start_round.ogg");
-    const roundMusic = new Audio("/audio/round_music.wav");
-    const endRound = new Audio("/audio/end_round.wav");
+    countdown.current = new Audio("/audio/countdown.mp3");
+    roundStart.current = new Audio("/audio/start_round.ogg");
+    roundMusic.current = new Audio("/audio/round_music.wav");
+    endRound.current = new Audio("/audio/end_round.wav");
 
     const handleGameStart = () => {
-      countdown.play();
+      if (countdown.current) countdown.current.play();
       setTimeout(() => {
-        countdown.pause();
-        roundStart.play();
+        if (countdown.current) countdown.current.pause();
+        if (roundStart.current) roundStart.current.play();
+        setIntermission(false);
         setGameStarted(true);
         handleRoundMusic();
       }, 5000);
     };
 
     const handleRoundMusic = () => {
-      roundMusic.play();
+      setRoundStarted(true);
+      if (roundMusic.current) roundMusic.current.play();
       setTimeout(() => {
-        roundMusic.pause();
-        endRound.play();
+        if (roundMusic.current) roundMusic.current.pause();
+        if (endRound.current) endRound.current.play();
+        setRoundOver(true);
+        setRoundStarted(false);
+        setTimeout(() => {
+          showLeaderboard();
+        }, 5000);
       }, 15000);
+    };
+
+    const showLeaderboard = () => {
+      setRoundOver(false);
+      setIntermission(true);
+      setTimeout(() => {
+        socket.emit("end_round", room);
+        handleGameStart();
+      }, 5000);
     };
 
     socket.on("game_started", handleGameStart);
@@ -33,7 +57,7 @@ export const useSoundEffects = () => {
     return () => {
       socket.off("game_started", handleGameStart);
     };
-  }, []);
+  }, [room]);
 
-  return { gameStarted };
+  return { gameStarted, roundStarted, roundOver, intermission };
 };
