a00d540c12ee2099138989da21d5061d49ebb340
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-24 17:57:04 -0400

    component to display countdown

---
+++ client/src/components/Countdown.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+interface CountdownProps {
+  time: number;
+}
+
+const Countdown = ({ time }: CountdownProps) => {
+  const [timer, setTimer] = useState(time);
+
+  useEffect(() => {
+    if (timer <= 0) return;
+    const intervalId = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, [timer]);
+
+  return (
+    <p className="text-center font-madimi font-bold text-soft-orange">
+      {timer}
+    </p>
+  );
+};
+
+export default Countdown;
