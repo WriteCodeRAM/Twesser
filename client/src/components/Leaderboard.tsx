f1cf4d3847196c421769c58b5faa1c3eff32344e
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-29 12:39:59 -0400

    added styling to members on leaderboards

--- client/src/components/Leaderboard.tsx
+++ client/src/components/Leaderboard.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { LobbyMembersProps } from "@/types";
 
-function Leaderboard() {
-  return <div>Leaderboard</div>;
+const textColors = ["text-gold", "text-platinum", "text-bronze"];
+const borderColors = ["border-gold", "border-platinum", "border-bronze"];
+const regular =
+  "border-rounded mb-3 flex justify-between gap-4 rounded-lg border-2 p-4 text-center font-madimi  ";
+
+function Leaderboard({ members }: LobbyMembersProps) {
+  return (
+    <div className="rounded-lg bg-black p-16">
+      <h2 className="mb-3 text-center font-madimi text-2xl text-light-blue">
+        Leaderboard
+      </h2>
+      {members.map((member, idx) => (
+        <div
+          key={idx}
+          className={
+            idx < 3 ? regular + borderColors[idx] : regular + "border-muted-red"
+          }
+        >
+          <span className={idx < 3 ? textColors[idx] : `text-white`}>
+            {member.name.toUpperCase()}
+          </span>
+          <span className={idx < 3 ? textColors[idx] : `text-white`}>
+            {member.score}
+          </span>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Leaderboard;
