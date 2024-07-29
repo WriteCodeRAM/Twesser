9cdefbf3b1f4f7b98a8553063ffa2c53b04a0562
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-29 12:37:34 -0400

    added listener to play submit sound onClick and diff colors for each answer choice

--- client/src/components/AnswerChoices.tsx
+++ client/src/components/AnswerChoices.tsx
@@ -1,4 +1,8 @@
 import AnswerChoiceButton from "./AnswerChoiceButton";
+import { socket } from "@/app/socket";
+import { useState, useEffect } from "react";
+import { useSoundEffects } from "@/app/hooks/useSoundEffects";
+import { AnswerChoicesProps } from "@/types";
 
 const colors: string[] = [
   "border-soft-orange bg-vibrant-teal",
@@ -6,12 +10,30 @@ const colors: string[] = [
   "border-muted-purple bg-muted-blue",
   "border-muted-blue bg-muted-purple",
 ];
+const AnswerChoices = ({ choices, answer, room }: AnswerChoicesProps) => {
+  const [disabled, setDisabled] = useState(false);
+  const { submitSound } = useSoundEffects(room);
 
-interface AnswerChoicesProps {
-  choices: string[];
-}
+  useEffect(() => {
+    socket.on("play_submit_sound", () => {
+      if (submitSound.current) submitSound.current.play();
+    });
 
-const AnswerChoices = ({ choices }: AnswerChoicesProps) => {
+    return () => {
+      socket.off("play_submit_sound");
+    };
+  }, [submitSound]);
+
+  // use onClick to set state disabled to true and send users answer to server
+  // need access to socket in here
+  // play sound from here and handle answer choice ???
+  function answerSubmission(choice: string) {
+    // console.log(`selected ${choice}`);
+    if (submitSound.current) submitSound.current.play();
+    setDisabled(true);
+
+    socket.emit("submit_answer", { room, choice, answer });
+  }
   return (
     <div className="grid grid-cols-2 grid-rows-2 gap-2">
       {choices.map(function (choice, idx) {
@@ -19,8 +41,9 @@ const AnswerChoices = ({ choices }: AnswerChoicesProps) => {
           <AnswerChoiceButton
             key={idx}
             name={choice}
-            onClick={() => console.log("clicked " + choice)}
+            onClick={() => answerSubmission(choice)}
             color={idx >= 2 ? (colors[idx] += " row-start-2") : colors[idx]}
+            disabled={disabled}
           />
         );
       })}
