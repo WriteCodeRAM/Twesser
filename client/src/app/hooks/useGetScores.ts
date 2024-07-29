abc1889814e309f6cffe0f61238264d38cffe6bc
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-29 12:28:04 -0400

    hook to fetch scores at end of rounds to then display on leaderboard component

---
+++ client/src/app/hooks/useGetScores.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+import { socket } from "../socket";
+import { LobbyMemberFields } from "@/types";
+
+export const useGetScores = (): LobbyMemberFields[] => {
+  const [scores, setScores] = useState<LobbyMemberFields[]>([]);
+
+  useEffect(() => {
+    const handleUpdatedScores = (newScores: LobbyMemberFields[]) => {
+      console.log("Received new scores:", newScores);
+      setScores(newScores);
+    };
+
+    socket.on("updated_scores", handleUpdatedScores);
+
+    return () => {
+      socket.off("updated_scores", handleUpdatedScores);
+    };
+  }, []);
+
+  return scores;
+};
