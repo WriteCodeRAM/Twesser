a9cc3f4e57fbf8a115135ac0ad2b5ae26e0265de
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-29 12:27:21 -0400

    imported all types used in components here

---
+++ client/src/types/index.ts
@@ -0,0 +1,41 @@
+export interface LobbyMemberFields {
+  id: string;
+  name: string;
+  host: boolean;
+  score: number;
+}
+
+export interface LobbyMembersProps {
+  members: LobbyMemberFields[];
+}
+
+export interface Question {
+  id: number;
+  answerChoices: string[];
+  blurredURL: string;
+  unblurredURL: string;
+  answer: string;
+}
+
+export interface LobbyScreenProps {
+  error: string;
+  data: Question | null;
+  gameStarted: boolean;
+  roundStarted: boolean;
+  roundOver: boolean;
+  intermission: boolean;
+  room: string;
+}
+
+export interface AnswerChoicesButtonProps {
+  name: string;
+  onClick: () => void;
+  color: string;
+  disabled: boolean;
+}
+
+export interface AnswerChoicesProps {
+  choices: string[];
+  answer: string;
+  room: string;
+}
