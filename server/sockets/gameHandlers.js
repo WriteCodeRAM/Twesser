77715d144fc8bfc7b2cf3e882c413c5a93832afe
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-07-29 12:46:31 -0400

    added point system and listers/emitters for handling submitted answers

--- server/sockets/gameHandlers.js
+++ server/sockets/gameHandlers.js
@@ -1,4 +1,12 @@
 module.exports = (io, socket, rooms) => {
+  const point_system = {
+    0: 100,
+    1: 90,
+    2: 80,
+    3: 70,
+    4: 60,
+    5: 50,
+  };
   socket.on("start_game", ({ room }) => {
     // find host
     const host = rooms[room].members.filter((member) => member.host === true);
@@ -15,7 +23,42 @@ module.exports = (io, socket, rooms) => {
       socket.emit("player_count_warning");
     }
   });
+
+  socket.on("submit_answer", ({ room, choice, answer }) => {
+    if (rooms[room]) {
+      const isCorrect = choice === answer;
+
+      const userIndex = rooms[room].members.findIndex(
+        (member) => member.id === socket.id
+      );
+      console.log(userIndex);
+      if (userIndex !== -1) {
+        if (isCorrect) {
+          rooms[room].members[userIndex].score +=
+            point_system[rooms[room].placement.length];
+          rooms[room].placement.push([
+            rooms[room].members[userIndex].name,
+            rooms[room].members[userIndex].score,
+          ]);
+        }
+      }
+
+      io.to(room).emit("play_submit_sound");
+    } else {
+      console.log(`Room ${room} not found`);
+    }
+  });
+
   socket.on("end_round", (room) => {
+    console.log(rooms[room].placement);
+    io.to(room).emit(
+      "updated_scores",
+      rooms[room].members.sort((a, b) => a.score > b.score)
+    );
+    rooms[room].placement = [];
+  });
+
+  socket.on("next_round", (room) => {
     io.to(room).emit("increment_index");
   });
 };
