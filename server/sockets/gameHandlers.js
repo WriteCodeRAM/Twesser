6029400cab1440048fd5917c05006c17d60680cb
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-08-08 01:28:04 -0400

    Fixed answer submission and implemented early round endings

--- server/sockets/gameHandlers.js
+++ server/sockets/gameHandlers.js
@@ -8,9 +8,10 @@ module.exports = (io, socket, rooms) => {
     if (host[0].id !== socket.id) {
       socket.emit("host_must_start_game");
     } else if (rooms[room] && rooms[room].members.length > 1) {
-      console.log("game started");
       // emit listener to room
+      rooms[room].indexIncrementedThisRound = false;
       io.to(room).emit("game_started");
+
       rooms[room].gameStarted = true;
     } else {
       console.log(`Game requires at least 2 people to play.`);
@@ -19,9 +20,11 @@ module.exports = (io, socket, rooms) => {
   });
 
   socket.on("submit_answer", ({ room, choice, answer, timer }) => {
+    rooms[room].responses.push(socket.id);
+    console.log(timer);
     if (rooms[room]) {
       const isCorrect = choice === answer;
-      console.log(timer);
+
       const userIndex = rooms[room].members.findIndex(
         (member) => member.id === socket.id
       );
@@ -39,19 +42,59 @@ module.exports = (io, socket, rooms) => {
       }
 
       io.to(room).emit("play_submit_sound");
+      if (rooms[room].responses.length == rooms[room].members.length) {
+        // end round early
+        io.to(room).emit("all_answers_submitted");
+      }
     } else {
       console.log(`Room ${room} not found`);
     }
   });
 
-  socket.on("end_round", (room) => {
-    const sortedMembers = rooms[room].members.sort((a, b) => b.score - a.score);
+  function incrementRoundIndex(room) {
+    if (!rooms[room].indexIncrementedThisRound) {
+      rooms[room].currentIndex++;
+      rooms[room].indexIncrementedThisRound = true;
+    }
+  }
 
-    io.to(room).emit("updated_scores", sortedMembers);
-    rooms[room].placement = [];
+  function startNewRound(room) {
+    rooms[room].indexIncrementedThisRound = false;
+    io.to(room).emit("increment_index", rooms[room].currentIndex);
+    io.to(room).emit("start_next_round");
+  }
+
+  socket.on("end_round", (room) => {
+    if (rooms[room]) {
+      const sortedMembers = rooms[room].members.sort(
+        (a, b) => b.score - a.score
+      );
+      io.to(room).emit("updated_scores", sortedMembers);
+      rooms[room].placement = [];
+      rooms[room].responses = [];
+
+      incrementRoundIndex(room);
+      console.log(rooms[room].currentIndex);
+      if (rooms[room].currentIndex < rooms[room].questions.length - 1) {
+        console.log(
+          `Preparing for next round. Current index: ${rooms[room].currentIndex}`
+        );
+
+        setTimeout(() => {
+          startNewRound(room);
+        }, 15000);
+      } else {
+        console.log("Game ended. All questions answered.");
+        io.to(room).emit("game_ended");
+      }
+    }
   });
 
-  socket.on("next_round", (room) => {
-    io.to(room).emit("increment_index");
+  socket.on("game_ended", (room) => {
+    if (rooms[room]) {
+      rooms[room].gameStarted = false;
+      io.to(room).emit("game_ended");
+      console.log("game over");
+    }
   });
 };
