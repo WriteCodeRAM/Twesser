78805afd31575e62d01abe90544e81098cbc6ab0
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-08-25 15:50:02 -0400

    Prepare for Render deployment
    - Add Node.js version specification to server package.json
    - Configure server to serve static files from Next.js build
    - Update Next.js config for static export

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const { createServer } = require("http");
 const setupSocket = require("./sockets");
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 const httpServer = createServer(app);
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "../client/out")));
 
 setupSocket(httpServer);
 
