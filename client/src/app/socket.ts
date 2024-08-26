744865944b60e6983e91f664a92097ec9e87f573
WriteCodeRAM <89037080+WriteCodeRAM@users.noreply.github.com>
2024-08-26 15:50:21 -0400

    Fix: export in socket config client side

--- client/src/app/socket.ts
+++ client/src/app/socket.ts
@@ -1,9 +1,7 @@
 import io from "socket.io-client";
 
-const socket = io(
+export const socket = io(
   process.env.NODE_ENV === "production"
     ? "https://twesser.onrender.com"
     : "http://localhost:10000",
 );
-
-export default socket;
